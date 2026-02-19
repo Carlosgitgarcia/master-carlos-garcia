33b49cd34cde92a3a82caaf702d2cfd65b40a97e
Carlosgitgarcia <carlosgc83@gmail.com>
2026-02-19 17:10:15 +0100

    eliminar web básica

--- carpeta/assets/footer.php
+++
@@ -1,5 +0,0 @@
-   <footer>
-              <p>Carlos García, SEO lo que SEA en 2025</p><a href= "mailto:carlos@seoloquesea.com">Puedes escribirme aquí</a>
-            </footer>
-    </body>
-</html>
