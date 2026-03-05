374dcc22eb99bde8028a21aedeb0d970c39ab3f3
Carlosgitgarcia <carlosgc83@gmail.com>
2026-03-05 18:45:39 +0100

    meta etiqueta página única
    ejercicios de página meta etiqueta

---
+++ unica.php
@@ -0,0 +1,20 @@
+ <!DOCTYPE html>
+ <html lang="es">
+        <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página para meta etiquetas - Máster SEO técnico</title>
+    <meta name="description" content="el error es que no has cerrado la etiqueta footer">
+    <meta name="robots" content="index, follow">
+        </head>
+        <body>
+            <header>            
+            <nav></nav>
+             </header>
+            <H1>página única</H1>
+
+            <footer>
+                 <p>Copyright</p>
+            </footer>
+
+        </body>
