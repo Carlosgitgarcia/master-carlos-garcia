9a89acb36e238cdc899309215b767f2598a111c1
Carlosgitgarcia <carlosgc83@gmail.com>
2026-03-06 11:29:30 +0100

    Ejercicios de meta etiquetas 2

--- unica.php
+++ unica.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página para meta etiquetas - Máster SEO técnico</title>
+    <meta name="googlebot" content="index, follow">
     <meta name="description" content="el error es que no has cerrado la etiqueta footer">
-    <meta name="robots" content="index, follow">
+    <meta name="robots" content="max-snippet:25">
+    <meta name="robots" content="noimageindex">
+    <meta name="robots" content="unavailable_after: 2033-09-27">
+    <meta http-equiv="refresh" content="30; url=https://google.com/" />
+
+    
         </head>
         <body>
             <header>            
